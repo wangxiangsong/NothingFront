eba0b6e35da455998a931330ff906b743fb5c098
wangxiangsong <xiangsong.wang@xuncetech.com>
2022-06-21 23:03:03 +0800

    feat: 随便谢谢...

--- src/pages/home/BottomBar/index.tsx
+++ src/pages/home/BottomBar/index.tsx
@@ -4,10 +4,51 @@ import styles from './index.module.less';
 import dayjs from 'dayjs';
 
 const TopBar: FC = () => {
-  const [date, setState] = useState<number>(new Date().getTime());
+  const [date, setState] = useState<any>();
   const counter = setInterval(() => {
-    setState(date + 1000);
+    setState(timeNow());
   }, 1000);
+
+  function timeNow() {
+    let vWeek, vWeek_s, year, month, day, hours, minutes, seconds;
+    vWeek = [
+      '星期天',
+      '星期一',
+      '星期二',
+      '星期三',
+      '星期四',
+      '星期五',
+      '星期六',
+    ];
+    let date = new Date();
+    year = date.getFullYear();
+    month = date.getMonth() + 1;
+    day = date.getDate();
+    hours = date.getHours();
+    hours = hours > 9 ? hours : '0' + hours;
+    minutes = date.getMinutes();
+    minutes = minutes > 9 ? minutes : '0' + minutes;
+    seconds = date.getSeconds();
+    seconds = seconds > 9 ? seconds : '0' + seconds;
+    vWeek_s = date.getDay();
+    let time =
+      year +
+      '年' +
+      month +
+      '月' +
+      day +
+      '日' +
+      '\t' +
+      hours +
+      ':' +
+      minutes +
+      ':' +
+      seconds +
+      '\t' +
+      vWeek[vWeek_s];
+    return time;
+  }
+
   useEffect(() => {
     return () => {
       clearInterval(counter);
@@ -16,9 +57,7 @@ const TopBar: FC = () => {
 
   return (
     <div className={styles['bottom-content']}>
-      <span>{`反正不是叙利亚时间：${dayjs(date).format(
-        'YYYY-MM-DD HH:MM:ss',
-      )}`}</span>
+      {date ? <span>{`反正不是叙利亚时间：${date}`}</span> : null}
     </div>
   );
 };
--- src/pages/homePage/index.tsx
+++ src/pages/homePage/index.tsx
@@ -1,10 +1,13 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import styles from './index.module.less';
 
 const HomePages: FC = () => {
   return (
     <div className={styles['homePage']}>
-      {/* <div className={styles['homePage_title']}>常用功能</div> */}
+      <div className={styles['homePage_title']}>
+        <span></span>
+        常用功能
+      </div>
       <div className={styles['homePage_content']}></div>
     </div>
   );
