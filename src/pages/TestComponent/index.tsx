da90aa1455ce60e859bcf23b188cff5462df5dd0
wangxiangsong <951606897@qq.com>
2025-10-15 09:47:06 +0800

    fix: 修改启动项

---
+++ src/pages/TestComponent/index.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+interface TestComponentProps {}
+
+const TestComponent: FC<TestComponentProps> = () => {
+  return <div>TestComponent</div>;
+};
+
+export default TestComponent;
